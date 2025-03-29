34a46155dfc693e713978f91a4e3d3ff901b4500
Onkar Ijare <ijareonkar2184@gmail.com>
2025-03-29 22:45:05 +0530

    react integration

--- backend/serveplus/settings.py
+++ backend/serveplus/settings.py
@@ -55,10 +55,19 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'serveplus.urls'
 
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',  # React's development server
+    # Add other allowed origins
+]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 'DIRS': [
+            # os.path.join(BASE_DIR, 'frontend/build'),
+        # ],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend', 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,6 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend', 'build', 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
